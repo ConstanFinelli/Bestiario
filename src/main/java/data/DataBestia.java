d9478fa10712a3ca2bb3796d7db9bbc920a8a3ac
RicardoLCost <costamagnarl@gmail.com>
2025-07-31 22:42:35 -0300

    Crud bestia inicio

--- src/main/java/data/DataBestia.java
+++ src/main/java/data/DataBestia.java
@@ -1,25 +1,28 @@
 package data;
 
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.LinkedList;
 
-import entities.Categoria;
+import entities.*;
 
-public class DataCategoria {
-
-	public Categoria getOne(Categoria catB) {
+public class DataBestia {
+	public Bestia getOne(Bestia b) {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		Categoria categoriaEncontrada = null;
+		Bestia bestiaEncontrada = null;
 		try {
-			pstmt = DbConnector.getInstancia().getConn().prepareStatement("select * from categoria where idCategoria = ?");
-			pstmt.setInt(1, catB.getIdCategoria());
+			pstmt = DbConnector.getInstancia().getConn().prepareStatement("select * from bestia where idBestia = ?");
+			pstmt.setInt(1, b.getIdBestia());
 			rs = pstmt.executeQuery();
 			if(rs != null && rs.next()) {
-				int id = rs.getInt("idCategoria");
+				int id = rs.getInt("idBestia");
 				String nombre = rs.getString("nombre");
-				String desc = rs.getString("descripcion");
-				categoriaEncontrada = new Categoria(id, nombre, desc);
+				String peligrosidad = rs.getString("peligrosidad");
+				bestiaEncontrada = new Bestia(id, nombre, peligrosidad);
+				completarBestia(bestiaEncontrada);
 			}
 		}catch(SQLException ex) {
 			System.out.println("Mensaje: " + ex.getMessage());
@@ -40,24 +43,25 @@ public class DataCategoria {
 	            System.out.println("Error del proveedor (VendorError): " + ex.getErrorCode());
 			}
 		}
-		return categoriaEncontrada;
+		return bestiaEncontrada;
 	}
 	
-	public LinkedList<Categoria> findAll(){
+	public LinkedList<Bestia> findAll(){
 		Statement stmt = null;
 		ResultSet rs = null;
-		Categoria categoria = null;
-		LinkedList<Categoria> categorias = new LinkedList<>();
+		Bestia bestia = null;
+		LinkedList<Bestia> bestias = new LinkedList<>();
 		try {
 			stmt = DbConnector.getInstancia().getConn().createStatement();
-			rs = stmt.executeQuery("Select * from categoria");
+			rs = stmt.executeQuery("Select * from bestia");
 			if(rs != null) {
 				while(rs.next()) {
-					int id = rs.getInt("idCategoria");
+					int id = rs.getInt("idBestia");
 					String nombre = rs.getString("nombre");
-					String desc = rs.getString("descripcion");
-					categoria = new Categoria(id, nombre, desc);
-					categorias.add(categoria);
+					String peligrosidad = rs.getString("peligrosidad");
+					bestia = new Bestia(id, nombre, peligrosidad);
+					completarBestia(bestia);
+					bestias.add(bestia);
 				}
 			}
 		}catch(SQLException ex) {
@@ -79,16 +83,15 @@ public class DataCategoria {
 	            System.out.println("Error del proveedor (VendorError): " + ex.getErrorCode());
 			}
 		}
-		return categorias;
+		return bestias;
 	}
 	
 	public Categoria save(Categoria cat) {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
-			pstmt = DbConnector.getInstancia().getConn().prepareStatement("insert into categoria(nombre, descripcion) values(?, ?)", PreparedStatement.RETURN_GENERATED_KEYS);
-			pstmt.setString(1, cat.getNombre());
-			pstmt.setString(2, cat.getDescripcionCategoria());
+			pstmt = DbConnector.getInstancia().getConn().prepareStatement("insert into Bestia(descripcion) values(?)", PreparedStatement.RETURN_GENERATED_KEYS);
+			pstmt.setString(1, cat.getDescripcionCategoria());
 			pstmt.executeUpdate();
 			rs = pstmt.getGeneratedKeys();
 			if(rs != null && rs.next()) {
@@ -119,10 +122,9 @@ public class DataCategoria {
 	public Categoria update(Categoria cat) {
 		PreparedStatement pstmt = null;
 		try {
-			pstmt = DbConnector.getInstancia().getConn().prepareStatement("update categoria set nombre = ?, descripcion = ? where idCategoria = ?");
-			pstmt.setString(1, cat.getNombre());
-			pstmt.setString(2, cat.getDescripcionCategoria());
-			pstmt.setInt(3, cat.getIdCategoria());
+			pstmt = DbConnector.getInstancia().getConn().prepareStatement("update categoria set descripcion = ? where idCategoria = ?");
+			pstmt.setString(1, cat.getDescripcionCategoria());
+			pstmt.setInt(2, cat.getIdCategoria());
 			int error = pstmt.executeUpdate();
 			if(error == 0) {
 				cat = null;
@@ -169,6 +171,55 @@ public class DataCategoria {
 			}
 		}
 		return catBorrada;
+	}
+	
+	public void addCategorias(Bestia bestiaEncontrada){
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = DbConnector.getInstancia().getConn().prepareStatement("select * from categoria c join bestia_categoria bc on bc.idCategoria = c.idCategoria where bc.idBestia = ?");
+			pstmt.setInt(1, bestiaEncontrada.getIdBestia());
+			rs = pstmt.executeQuery();
+			if(rs != null) {
+				while(rs.next()) {
+					int id = rs.getInt("idCategoria");
+					String nombre = rs.getString("nombre");
+					String desc = rs.getString("descripcion");
+					Categoria categoria = new Categoria(id, nombre, desc);
+					bestiaEncontrada.getCategorias().add(categoria);
+				}
+			}
+		}catch(SQLException ex) {
+			System.out.println("Mensaje: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("Error del proveedor (VendorError): " + ex.getErrorCode());
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				DbConnector.getInstancia().releaseConn();
+			}catch(SQLException ex) {
+				System.out.println("Mensaje: " + ex.getMessage());
+	            System.out.println("SQLState: " + ex.getSQLState());
+	            System.out.println("Error del proveedor (VendorError): " + ex.getErrorCode());
+				}
+			}
+		}
+	public void addHabitats(Bestia bestiaEncontrada) {
+		
+	}
+	public void addRegistros(Bestia bestiaEncontrada) {
+		
+	}
+	
+	public void completarBestia(Bestia bestia) {
+		addRegistros(bestia);
+		addHabitats(bestia);
+		addCategorias(bestia);
 	}
 }
 
---
+++ src/main/java/entities/Bestia.java
@@ -0,0 +1,78 @@
+package entities;
+
+import java.util.LinkedList;
+
+public class Bestia {
+	private int idBestia;
+	private String nombre;
+	private String peligrosidad;
+	private LinkedList<Habitat> habitats = new LinkedList<>();
+	private LinkedList<Categoria> categorias  = new LinkedList<>();
+	private LinkedList<Registro> registros  = new LinkedList<>();
+	
+	public Bestia(int id, String name, String danger) {
+		idBestia = id;
+		nombre = name;
+		peligrosidad = danger;
+	}
+	
+	public int getIdBestia() {
+		return idBestia;
+	}
+	public void setIdBestia(int idBestia) {
+		this.idBestia = idBestia;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPeligrosidad() {
+		return peligrosidad;
+	}
+	public void setPeligrosidad(String peligrosidad) {
+		this.peligrosidad = peligrosidad;
+	}
+	public LinkedList<Habitat> getHabitats(){
+		return habitats;
+	}
+	public LinkedList<Categoria> getCategorias(){
+		return categorias;
+	}
+	public LinkedList<Registro> getRegistros(){
+		return registros;
+	}
+	public String listarHabitats() {
+		String lista = "";
+		for(Habitat hab: habitats) {
+			lista = lista + "<br>" + hab.getNombre() + ",";
+		}
+		return lista;
+	}
+
+	public String listarCategorias() {
+		String lista = "";
+		for(Categoria cat: categorias) {
+			lista = lista + "<br>" + cat.getDescripcionCategoria() + ",";
+		}
+		return lista;
+	}
+	public String listarRegistros() {
+		String lista = "";
+		for(Registro reg: registros) {
+			lista = lista + "<br>" + reg.getNroRegistro() + reg.getDetalles() + reg.getFechaAprobacion().toString() +  reg.getEstado() + 
+					reg.getFechaBaja().toString() + reg.getPublicador().getIdUsuario();
+		}
+		return lista;
+	}
+	
+	 @Override public String toString(){
+		 return("Id de Bestia: " + idBestia + 
+				 "<br>Nombre: " + nombre + 
+				 "<br>Peligrosidad: " + peligrosidad+
+				 "<br>" + listarHabitats() + 
+				 "<br>" + listarCategorias() + 
+				 "<br>" + listarRegistros());
+	 }
+}
