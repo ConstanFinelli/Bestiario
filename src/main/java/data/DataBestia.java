f4f38ca10c38d996e464ec806066bf616bdbe018
RicardoLCost <costamagnarl@gmail.com>
2025-08-02 15:20:19 -0300

    CONTINUA CRUD registro

--- src/main/java/data/DataBestia.java
+++ src/main/java/data/DataBestia.java
@@ -4,11 +4,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.LinkedList;
 
 import entities.*;
+import logic.*;
 
 public class DataBestia {
+	public LogicHabitat controladorHabitat = new LogicHabitat();
+	public LogicCategoria controladorCategoria = new LogicCategoria();
+	public LogicRegistro controladorRegistro = new LogicRegistro();
+	
 	public Bestia getOne(Bestia b) {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -177,15 +183,13 @@ public class DataBestia {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
-			pstmt = DbConnector.getInstancia().getConn().prepareStatement("select * from categoria c join bestia_categoria bc on bc.idCategoria = c.idCategoria where bc.idBestia = ?");
+			pstmt = DbConnector.getInstancia().getConn().prepareStatement("select distinct idCategoria from categoria c join bestia_categoria bc on bc.idCategoria = c.idCategoria where bc.idBestia = ?");
 			pstmt.setInt(1, bestiaEncontrada.getIdBestia());
 			rs = pstmt.executeQuery();
 			if(rs != null) {
 				while(rs.next()) {
-					int id = rs.getInt("idCategoria");
-					String nombre = rs.getString("nombre");
-					String desc = rs.getString("descripcion");
-					Categoria categoria = new Categoria(id, nombre, desc);
+					Categoria categoria = new Categoria(rs.getInt("idCategoria"),null, null);
+					categoria = controladorCategoria.getOne(categoria);
 					bestiaEncontrada.getCategorias().add(categoria);
 				}
 			}
@@ -210,10 +214,72 @@ public class DataBestia {
 			}
 		}
 	public void addHabitats(Bestia bestiaEncontrada) {
-		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = DbConnector.getInstancia().getConn().prepareStatement("select distinct idHabitat from habitat h join bestia_habitat bh on bh.idHabitat = h.idHabitat where bh.idBestia = ?");
+			pstmt.setInt(1, bestiaEncontrada.getIdBestia());
+			rs = pstmt.executeQuery();
+			if(rs != null) {
+				while(rs.next()) {
+					Habitat habitat = new Habitat(rs.getInt("idHabitat"), null, null, null);
+					habitat = controladorHabitat.getOne(habitat);
+					bestiaEncontrada.getHabitats().add(habitat);
+				}
+			}
+		}catch(SQLException ex) {
+			System.out.println("Mensaje: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("Error del proveedor (VendorError): " + ex.getErrorCode());
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				DbConnector.getInstancia().releaseConn();
+			}catch(SQLException ex) {
+				System.out.println("Mensaje: " + ex.getMessage());
+	            System.out.println("SQLState: " + ex.getSQLState());
+	            System.out.println("Error del proveedor (VendorError): " + ex.getErrorCode());
+				}
+			}
 	}
 	public void addRegistros(Bestia bestiaEncontrada) {
-		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = DbConnector.getInstancia().getConn().prepareStatement("select distinct nroRegistro from Registros where idBestia = ?");
+			pstmt.setInt(1, bestiaEncontrada.getIdBestia());
+			rs = pstmt.executeQuery();
+			if(rs != null) {
+				while(rs.next()) {
+					Registro registro = new Registro(rs.getInt("nroRegistro"), bestiaEncontrada);
+					registro = controladorRegistro.getOne(registro);
+					bestiaEncontrada.getRegistros().add(registro);
+				}
+			}
+		}catch(SQLException ex) {
+			System.out.println("Mensaje: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("Error del proveedor (VendorError): " + ex.getErrorCode());
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				DbConnector.getInstancia().releaseConn();
+			}catch(SQLException ex) {
+				System.out.println("Mensaje: " + ex.getMessage());
+	            System.out.println("SQLState: " + ex.getSQLState());
+	            System.out.println("Error del proveedor (VendorError): " + ex.getErrorCode());
+				}
+			}
 	}
 	
 	public void completarBestia(Bestia bestia) {
