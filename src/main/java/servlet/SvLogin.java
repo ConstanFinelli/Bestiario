c4b8b417ee72bb903d0f2875ef202c02849ebf5e
ConstanFinelli <constanfinelli@gmail.com>
2025-09-04 11:32:29 -0300

    Partially implemented login functionality

--- src/main/java/servlet/SvLogin.java
+++ src/main/java/servlet/SvLogin.java
@@ -1,41 +1,57 @@
 package servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import logic.LogicUsuario;
+
 import java.io.IOException;
 
-/**
- * Servlet implementation class SvLogin
- */
+import entities.Usuario;
+
 @WebServlet("/SvLogin")
 public class SvLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    public LogicUsuario controladorUsuario = new LogicUsuario();   
+	
     public SvLogin() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+    	rd.forward(request, response);
+    }
+    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+		String correo = request.getParameter("correo");
+		String contrasena = request.getParameter("contrasena");
+		String logMsg = "";
+		
+		
+		Usuario usuario = controladorUsuario.getByEmail(correo);
+		
+		if(usuario != null) {
+			if(contrasena.equals(usuario.getContraseña())) {
+				HttpSession session = request.getSession();
+	            session.setAttribute("user", usuario);
+	            
+	            response.sendRedirect("home.html");
+	            return;
+			}else {
+				logMsg = "Contraseña incorrecta";
+			}
+		}else {
+			logMsg = "Usuario no encontrado";
+		}
+		request.setAttribute("logMsg", logMsg);
+		rd.forward(request, response);
 	}
 
 }
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Bestiario - Login</title>
+<link rel="stylesheet" href="login.css">
+</head>
+<body class='logBody'>
+        <form class="logForm" action="SvLogin" method="POST">
+            <header class="logContainerHeader">
+                <h1 class="logContainerHeaderText">Inicio de sesión</h1>
+            </header>
+            <section class="logInputs">
+                <div class="logInput">
+                    <input type="email" required placeholder=' ' id="correo" name="correo"/>
+                    <label for='correo'>✉️ Correo electrónico</label>
+                </div>
+                <div class="logInput">
+                    <input type="password" required placeholder=' ' id="contrasena" name='contrasena'/>
+                    <label for="contrasena">🔐 Contraseña</label>
+                </div>
+                <div class='otherMsgs'>
+                    <span class='otherMsg'>¿Has olvidado tu contraseña?</span>
+                    <span class='otherMsg'>¿Desea registrarse?</span>
+                </div>
+            </section>
+            <aside class="logSubmitContainer">
+                <input type="submit" class="logSubmit" value="Iniciar sesión" />
+            </aside>
+            <div class='errorBox'>
+                    <ul>
+                        <li class="errorMsg">${logMsg}</li>
+                    </ul>
+            </div>
+        </form>
+</body>
+</html>
