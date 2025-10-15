ce411a8dcef058e09f7eae61578ec594d87a655e
ConstanFinelli <constanfinelli@gmail.com>
2025-10-15 13:00:04 -0300

    changed bestia-evidencia to bestia_evidencia

--- docs/bestiario.sql
+++ docs/bestiario.sql
@@ -48,7 +48,7 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `bestia_evidencia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `bestia-evidencia` (
+CREATE TABLE `bestia_evidencia` (
   `nroEvidencia` int NOT NULL,
   `idBestia` int NOT NULL,
   `detalle` varchar(45) DEFAULT NULL,
