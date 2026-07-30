512c4b3a182a9c0a35f63b61e30de92d62a6b5ee
ConstanFinelli <constanfinelli@gmail.com>
2026-07-30 19:26:04 -0300

    logger for AgregarComentario

--- src/main/java/servlet/comentario/AgregarComentario.java
+++ src/main/java/servlet/comentario/AgregarComentario.java
@@ -8,9 +8,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import logic.LogicBestia;
 import logic.LogicComentario;
 import logic.LogicUsuario;
+import servlet.categoria.EliminarCategoria;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import entities.Bestia;
 import entities.Comentario;
@@ -25,6 +30,7 @@ public class AgregarComentario extends HttpServlet {
 	private LogicComentario controladorComentario = new LogicComentario();
 	private LogicBestia controladorBestia = new LogicBestia();
 	private LogicUsuario controladorUsuario = new LogicUsuario();
+	private static final Logger logger = Logger.getLogger(AgregarComentario.class.getName());
 	
 	private static final long serialVersionUID = 1L;
        
@@ -41,12 +47,35 @@ public class AgregarComentario extends HttpServlet {
 		String idUsuario = request.getParameter("idUsuario");
 		String idBestia = request.getParameter("idBestia");
 		String nroRegistro = request.getParameter("nroRegistro");
+		List<String> errores = new ArrayList<>();
+		
 		if(contenido != null && idUsuario != null && idBestia != null) {
 			LocalDateTime fechaComentario = LocalDateTime.now();
-			Usuario publicador = controladorUsuario.getOne(new Usuario(Integer.parseInt(idUsuario)));
-			Bestia bestia = controladorBestia.getOne(new Bestia(Integer.parseInt(idBestia),null,null, null));
+			Usuario publicador = null;
+			Bestia bestia = null;
+			try {
+				publicador = controladorUsuario.getOne(new Usuario(Integer.parseInt(idUsuario)));
+			}catch(Exception e) {
+				logger.log(Level.WARNING, "Error al conseguir publicador del comentario a agregar en el servlet AgregarComentario", e);
+				errores.add("No se ha podido conseguir el publicador del comentario a agregar");
+			}
+			try {
+				bestia = controladorBestia.getOne(new Bestia(Integer.parseInt(idBestia),null,null, null));
+			}catch(Exception e) {
+				logger.log(Level.WARNING, "Error al conseguir la bestia asociada al comentario a agregar en el servlet AgregarComentario", e);
+				errores.add("No se ha podido conseguir la bestia asociada al comentario a agregar");
+			}
 			Comentario comentario = new Comentario(publicador, bestia, fechaComentario, contenido);
-			controladorComentario.save(comentario);
+			try {
+				controladorComentario.save(comentario);
+			}catch(Exception e) {
+				logger.log(Level.WARNING, "Error al crear comentario en el servlet AgregarComentario", e);
+				errores.add("No se ha podido crear el comentario");
+			}
+		}
+		if(!errores.isEmpty()) {
+			errores.add("Por favor, intente mas tarde");
+			request.setAttribute("errorGlobal", errores);
 		}
 		response.sendRedirect(HttpRoutes.OBTENER_REGISTRO_BESTIA(request.getContextPath()) + "?id=" + idBestia + "&nroRegistro=" + nroRegistro + "#comentarios");
 	}
